6faab3a9e5eb40d17fbd307256252e822fad28fc
Filipe Lima <cimofelipe4@gmail.com>
2022-08-30 17:19:13 -0300

    Checkpoint req 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    valueInput: '',
+    disabled: true,
+  };
+
+  verifyInput = ({ target }) => {
+    this.setState({ valueInput: target.value }, () => {
+      const { valueInput } = this.state;
+      const minLength = 3;
+      if (valueInput.length >= minLength) {
+        this.setState({ disabled: false });
+      }
+    });
+  };
+
   render() {
+    const { valueInput, disabled } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            placeholder="Informe seu nome"
+            data-testid="login-name-input"
+            value={ valueInput }
+            onChange={ this.verifyInput }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ disabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
