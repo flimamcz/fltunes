e29a57627bca2d309361b81563eeb06a8184491d
Filipe Lima <cimofelipe4@gmail.com>
2022-08-31 10:24:17 -0300

    checkpoin req 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     valueInput: '',
     disabled: true,
+    loading: false,
+    dataMusic: [],
+    notFoundAlbum: false,
   };
 
   searchArtist = ({ target }) => {
@@ -19,27 +24,74 @@ class Search extends Component {
     });
   };
 
+  fetchAlbuns = async () => {
+    const { valueInput } = this.state;
+    this.setState({ loading: true }, async () => {
+      const responseAlbum = await searchAlbumsAPI(valueInput);
+      this.setState({
+        dataMusic: responseAlbum,
+        loading: false,
+      });
+
+      if (responseAlbum.length <= 0) {
+        this.setState({ notFoundAlbum: true });
+      }
+
+      // if (responseAlbum.length > 0) {
+      //   this.setState({ notFoundAlbum: false });
+      // }
+    });
+  };
+
   render() {
-    const { valueInput, disabled } = this.state;
+    const { valueInput, disabled, loading, dataMusic, notFoundAlbum } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form onSubmit={ (e) => e.preventDefault() }>
-          <input
-            type="text"
-            placeholder="Nome do artista"
-            value={ valueInput }
-            onChange={ this.searchArtist }
-            data-testid="search-artist-input"
-          />
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ disabled }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? <Loading /> : (
+          <form onSubmit={ (e) => e.preventDefault() }>
+            <input
+              type="text"
+              placeholder="Nome do artista"
+              value={ valueInput }
+              onChange={ this.searchArtist }
+              data-testid="search-artist-input"
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ disabled }
+              onClick={ this.fetchAlbuns }
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
+
+        <div className="artist">
+          {dataMusic.length ? (
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              { valueInput }
+            </p>
+          ) : null}
+
+          {dataMusic.length > 0 && dataMusic.map((album) => (
+            <div key={ album.collectionId }>
+              <img src={ album.artworkUrl100 } alt={ album.artistName } />
+              <p>
+                Album:
+                {' '}
+                {album.collectionName}
+              </p>
+              <p>{album.artistName}</p>
+              {console.log(album)}
+            </div>
+          ))}
+
+          {notFoundAlbum && <p>Nenhum álbum foi encontrado!</p>}
+        </div>
       </div>
     );
   }
