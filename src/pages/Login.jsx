94da38413e9829c3e6446d10a2d158742baa2c0f
Filipe Lima <cimofelipe4@gmail.com>
2022-08-30 21:16:53 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
   state = {
     valueInput: '',
     disabled: true,
+    loading: false,
+    loggedIn: false,
   };
 
   verifyInput = ({ target }) => {
@@ -12,30 +17,48 @@ class Login extends Component {
       const minLength = 3;
       if (valueInput.length >= minLength) {
         this.setState({ disabled: false });
+      } else {
+        this.setState({ disabled: true });
+      }
+    });
+  };
+
+  fetchCreateUser = async () => {
+    const { valueInput } = this.state;
+    this.setState({ loading: true }, async () => {
+      const responseCreateUser = await createUser({ name: valueInput });
+      this.setState({ loading: false });
+      if (responseCreateUser === 'OK') {
+        this.setState({ loggedIn: true });
       }
     });
   };
 
   render() {
-    const { valueInput, disabled } = this.state;
+    const { valueInput, disabled, loading, loggedIn } = this.state;
     return (
       <div data-testid="page-login">
-        <form>
-          <input
-            type="text"
-            placeholder="Informe seu nome"
-            data-testid="login-name-input"
-            value={ valueInput }
-            onChange={ this.verifyInput }
-          />
-          <button
-            type="button"
-            data-testid="login-submit-button"
-            disabled={ disabled }
-          >
-            Entrar
-          </button>
-        </form>
+        {loading ? <Loading /> : (
+          <form>
+            <input
+              type="text"
+              placeholder="Informe seu nome"
+              data-testid="login-name-input"
+              value={ valueInput }
+              onChange={ this.verifyInput }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ disabled }
+              onClick={ this.fetchCreateUser }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+
+        {loggedIn ? <Redirect to="/search" /> : null}
       </div>
     );
   }
