13980215f3b12308add9566a316a916e2b073b33
Filipe Lima <cimofelipe4@gmail.com>
2022-09-01 18:09:02 -0300

    Requisito 14 :check:

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    loading: false,
+    user: {},
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({
+        user,
+        loading: false,
+      });
+    });
+  };
+
   render() {
+    const { user, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {loading ? <Loading /> : (
+          <div>
+            <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+            <Link to="/profile/edit">Editar perfil</Link>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.description}</p>
+          </div>
+        )}
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,143 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
+  state = {
+    loading: false,
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+    buttonDisabled: true,
+    redirect: false,
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      const { name, email, description, image } = await user;
+      this.setState({
+        loading: false,
+        name,
+        email,
+        image,
+        description,
+      });
+    });
+  };
+
+  handleInput = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, () => {
+      const { name: nameInput, email, image, description } = this.state;
+      const isFilled = (nameInput.length > 0)
+        && (email.length > 0)
+        && (image.length > 0)
+        && (description.length > 0);
+      const regex = /\S+@\S+\.\S+/;
+      const isEmail = regex.test(email);
+      if (isFilled && isEmail) {
+        this.setState({ buttonDisabled: false });
+      } else this.setState({ buttonDisabled: true });
+    });
+  };
+
+  updateUser = async () => {
+    const { name, email, description, image } = this.state;
+    const userUpdate = {
+      name,
+      email,
+      description,
+      image,
+    };
+
+    this.setState({ redirect: false, loading: true }, async () => {
+      const response = await updateUser(userUpdate);
+      if (response === 'OK') {
+        this.setState({ redirect: true, loading: false });
+        this.props.history.push('/profile')
+      }
+    });
+  };
+
   render() {
+    const {
+      loading, name, email,
+      description, image,
+      buttonDisabled, redirect,
+    } = this.state;
+
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        {loading ? <Loading /> : (
+          <form>
+            <label htmlFor="name">
+              Nome
+              <span>Fique a vontade para usar seu nome social</span>
+              <input
+                type="text"
+                data-testid="edit-input-name"
+                name="name"
+                id="name"
+                value={ name }
+                onChange={ this.handleInput }
+              />
+            </label>
+
+            <label htmlFor="email">
+              Email
+              <span>Informe seu melhor email</span>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                data-testid="edit-input-email"
+                value={ email }
+                onChange={ this.handleInput }
+              />
+            </label>
+
+            <label htmlFor="description">
+              Descrição
+              <textarea
+                id="description"
+                cols="30"
+                name="description"
+                rows="5"
+                data-testid="edit-input-description"
+                value={ description }
+                onChange={ this.handleInput }
+              />
+            </label>
+
+            <input
+              type="text"
+              data-testid="edit-input-image"
+              placeholder="Insira um link"
+              name="image"
+              value={ image }
+              onChange={ this.handleInput }
+            />
+
+            <button
+              type="button"
+              data-testid="edit-button-save"
+              disabled={ buttonDisabled }
+              onClick={ this.updateUser }
+            >
+              Editar perfil
+            </button>
+          </form>
+        )}
+        {redirect && <Redirect to="/profile" />}
       </div>
     );
   }
